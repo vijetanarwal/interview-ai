61fc9bc53248393b0bbf5d43397fb08af8c96dfe
vijetanarwal <vijetanarwal4044@gmail.com>
2026-06-21 10:51:08 +0530

    fix cookie for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -38,7 +38,11 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
 
 
     res.status(201).json({
@@ -80,7 +84,11 @@ async function loginUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none"
+})
     res.status(200).json({
         message: "User loggedIn successfully.",
         user: {
